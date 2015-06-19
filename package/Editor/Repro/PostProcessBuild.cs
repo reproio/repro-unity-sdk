1560735cd2cf40343fb232b7987959e0544c885a
nekoe <nekoe@MBP.local>
2015-06-19 16:47:53 +0900

    add package

---
+++ package/Editor/Repro/PostProcessBuild.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using Repro.XCodeEditor;
+
+namespace Repro {
+	
+	public abstract class PostProcessBuild {
+
+		[PostProcessBuildAttribute(100)]
+		public static void OnPostprocessBuild (BuildTarget target, string pathToBuiltProject) {
+
+			XCProject project = new XCProject (pathToBuiltProject);
+
+			project.AddFrameworkSearchPaths ("$(PROJECT_DIR)/Frameworks/Plugins/iOS/Repro.embeddedframework");
+			project.AddOtherLDFlags ("-ObjC");
+
+			project.Save ();
+		}
+	}
+}
---
+++ package/Plugins/Repro/Repro.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+public class Repro {
+
+#if UNITY_IPHONE
+	[DllImport ("__Internal")]
+	private static extern void  _setup (string token);
+
+	[DllImport ("__Internal")]
+	private static extern void _startRecording ();
+#endif
+
+	public static void Setup (string token) {
+#if UNITY_IPHONE
+		_setup (token);
+#elif UNITY_ANDROID
+		Debug.Log( "Repro: Android is not yet supportd." );
+#endif
+	}
+
+	public static void StartRecording () {
+#if UNITY_IPHONE
+		_startRecording ();
+#elif UNITY_ANDROID
+		Debug.Log( "Repro: Android is not yet supportd." );
+#endif
+	}
+}
