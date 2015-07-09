9fcdb16515429af1ae03f3c3fcd77bfd53b8ccb0
morou <kazuhiro.nakae@gmail.com>
2015-07-09 13:09:32 +0900

    bump: 0.0.5

--- tools/build-unity-package.sh
+++ tools/build-unity-package.sh
@@ -3,4 +3,4 @@
 UNITY="/Applications/Unity4.0/Unity.app/Contents/MacOS/Unity"
 UNITY_PACKAGE="Repro.unitypackage"
 
-$UNITY -batchmode -quit -logFile build.log -projectPath $(pwd)/build/4 -exportPackage Assets/Editor Assets/Plugins $(pwd)/$UNITY_PACKAGE
+$UNITY -batchmode -quit -logFile build.log -projectPath $(pwd)/projects/4 -exportPackage Assets/Editor Assets/Plugins $(pwd)/$UNITY_PACKAGE
