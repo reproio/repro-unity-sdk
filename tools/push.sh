8b0b371e11967accad42abd662985dd326267014
morou <kazuhiro.nakae@gmail.com>
2015-07-09 12:57:04 +0900

    fix(tool): push tag

--- tools/push.sh
+++ tools/push.sh
@@ -5,6 +5,9 @@ COMMENT="bump: $NEW_VERSION"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 git add .
+
 git commit -m "$COMMENT"
-git tag -a "$NEW_VERSION" -m "$COMMENT"
 git push origin "$BRANCH"
+
+git tag -a "$NEW_VERSION" -m "$COMMENT"
+git push --tag
